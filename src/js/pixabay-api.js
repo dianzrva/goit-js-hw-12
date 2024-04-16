3d58680a168d2e92e462d23253d86b3d7152c3fc
dianzrva <diana.nazapowa64@gmail.com>
2024-04-16 20:21:41 +0200

    first commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,47 @@
+'use strict';
+import axios from 'axios';
+
+const pixabay = async (searchTerm, page, pageItems = 15) => {
+  console.log(`pixabay:search=>[${searchTerm}]`);
+  // const pageItems = 15;
+  const baseUrl = 'https://pixabay.com/api/';
+  const api_key = '43423774-a3cd1b17116146cc10b0d5ce8';
+  const queryParams = {
+    key: api_key,
+    q: searchTerm,
+    safesearch: true,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    per_page: pageItems,
+    page: page,
+  };
+
+  /*
+
+    const queryString = new URLSearchParams(queryParams).toString();
+    console.log('queryParams:', queryParams);
+    const url = `${baseUrl}?${queryString}`;
+    const options = {
+      method: 'GET',
+    };
+    axios.defaults.baseURL = 'https://pixabay.com';
+    //axios.defaults.baseURL = 'https://pixabay.com/api/';
+    const response = axios.get(`/api/?${queryString}`);
+    return response;
+  */
+
+  try {
+    const url = baseUrl;
+    const config = { params: queryParams };
+    const response = await axios.get(url, config);
+    return response;
+  } catch (error) {
+    console.error('pixabay:Error fetching images:', error);
+    throw error;
+  } finally {
+    console.log(`pixabay:search.end;`);
+  }
+};
+
+console.log('pixaBay.api:loaded...');
+export default pixabay;
